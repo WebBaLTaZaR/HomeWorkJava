728eaae5b8b59db8ae18c96a33543cfbd844e64e
BaLTaZaR <gricaev_sergey@mail.ru>
2023-08-17 16:54:50 +0600

    завершил задание

--- HomeWork.java
+++ HomeWork.java
@@ -1,48 +1,41 @@
 package HomeWorkJava;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class HomeWork {
 
-	/**
-	 * Дан текст. Нужно отсортировать слова по длине (по возрастанию) и вывести
-	 * статистику на экран.
-	 * Регистр слова не имеет значения. Формат вывода произвольный.
-	 * Программа-минимум:
-	 * 1. Слова, состоящие из дефисов, считаем одним словом. Т.е. каких-то - одно
-	 * слово из 8 символов.
-	 * 2. Точки и запятые не должны входить в статистику.
-	 *
-	 * Доп. задание
-	 * 1. * Не включать дефис в длину слова. Т.е. каких-то - одно слово из 7
-	 * символов.
-	 *
-	 * Пример:
-	 *
-	 * Это мой первый текст. Он состоит из каких-то тестовых слов и нужен для того,
-	 * чтобы выполнить тестовое задание GB.
-	 * Данный текст не несет в себе какого-либо смысла, он просто содержит набор
-	 * слов.
-	 *
-	 * 1 -> [и, в]
-	 * 2 -> [он, из, gb, не]
-	 * 3 -> [мой, для]
-	 * 4 -> [слов, того, себе]
-	 * 5 -> [текст, нужен, чтобы, несет, набор]
-	 * ...
-	 */
-	static void printStats(String text) {
-		// 1. Split текста, приведение его к нижнему регистру, удаление запятых и точек.
-		// 2. Сбор структуры со статистикой.
-		Map<Integer, List<String>> stats; // Структура, в которой ключ - длина слова, значение - список таких слов.
-	}
+    public static void printStats(String text) {
+        // 1. Преобразование текста: приведение к нижнему регистру и удаление запятых и точек.
+        text = text.replaceAll("[.,]", "").toLowerCase();
 
-	public static void main(String[] args) {
-		String text = "Это мой первый текст. Он состоит из каких-то тестовых слов и нужен для того, чтобы выполнить тестовое задание GB. "
-				+
-				"Данный текст не несет в себе какого-либо смысла, он просто содержит набор слов.";
+        // 2. Разделение текста на слова.
+        String[] words = text.split("\\s+");
 
-		printStats(text);
-	}
+        // 3. Сбор структуры со статистикой.
+        Map<Integer, List<String>> stats = new TreeMap<>(); // Используем TreeMap для автоматической сортировки ключей.
+
+        for (String word : words) {
+            if (!word.isEmpty() && !word.equals("-")) { // Пропускаем пустые строки и дефисы.
+                int length = word.length();
+                List<String> wordsWithSameLength = stats.getOrDefault(length, new ArrayList<>());
+                wordsWithSameLength.add(word);
+                stats.put(length, wordsWithSameLength);
+            }
+        }
+
+        // 4. Вывод статистики.
+        for (Map.Entry<Integer, List<String>> entry : stats.entrySet()) {
+            int length = entry.getKey();
+            List<String> wordList = entry.getValue();
+            System.out.println(length + " -> " + wordList);
+        }
+    }
+
+    public static void main(String[] args) {
+        String text = "Это мой первый текст. Он состоит из каких-то тестовых слов и нужен для того, чтобы " +
+                "выполнить тестовое задание GB. Данный текст не несет в себе какого-либо смысла, он просто содержит набор слов.";
+
+        printStats(text);
+    }
 }
+
